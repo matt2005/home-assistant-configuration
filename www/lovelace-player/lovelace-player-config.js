ab93490bc2cd6dbb8c498c0403c579a5f99409ff
Matt <matthilton2005@gmail.com>
2018-09-29 21:06:06 +0100

    added player and fully kisok files

--- www/lovelace-player/lovelace-player-config.js
+++ www/lovelace-player/lovelace-player-config.js
@@ -2,7 +2,7 @@ setTimeout(function() {
 
   // Add your bindings here
   // Example:
-  LovelacePlayer.bind("media_player.fire_dashboard", "f5c5b8d6-33ec161f");
+  LovelacePlayer.bind("media_player.fire_dashboard", "e371f2bc-67705236");
 
   // Remove this line when you found your Device ID
   LovelacePlayer.showDeviceId();
