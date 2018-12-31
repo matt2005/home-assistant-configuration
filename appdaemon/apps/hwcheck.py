c6b54204243c22c2d6494d7eab95dc1c79a69771
Matt <matthilton2005@gmail.com>
2018-12-31 00:04:58 +0000

    december updates post 0.84

--- appdaemon/apps/hwcheck.py
+++ appdaemon/apps/hwcheck.py
@@ -9,21 +9,21 @@ class HWCheck(hass.Hass):
     self.listen_event(self.appd_event, "appd_started")
     
   def ha_event(self, event_name, data, kwargs):
-    self.log_notify("Home Assistant is up", "INFO")
+    self.log_notify("Home Assistant is up: {}".format(self.args['namespace']), "INFO")
     self.run_in(self.hw_check, self.args["delay"])
     
   def appd_event(self, event_name, data, kwargs):
-    self.log_notify("AppDaemon is up", "INFO")
+    self.log_notify("AppDaemon is up: {}".format(self.args['namespace']), "INFO")
     
   def hw_check(self, kwargs):
     state = self.get_state()
     
     if "zwave" in self.args and self.args["zwave"] not in state:
-      self.log_notify("ZWAVE not started after delay period", "WARNING")
+      self.log_notify("ZWAVE not started after delay period: {}".format(self.args['namespace']), "WARNING")
     if "hue" in self.args and self.args["hue"] not in state:
-      self.log_notify("HUE not started after delay period", "WARNING")
+      self.log_notify("HUE not started after delay period: {}".format(self.args['namespace']), "WARNING")
     if "trafri" in self.args and self.args["tradfri"] not in state:
-      self.log_notify("tradfri not running", "WARNING")
+      self.log_notify("tradfri not running: {}".format(self.args['namespace']), "WARNING")
       
   def log_notify(self, msg, level):
     self.log(msg, level)
